6c7a5dbb6f4e46c6c00bf0f9a4705158f0429b3c
monofuel <monofuel@japura.net>
2023-03-18 15:53:52 -0700

    fmt

--- src/main.nim
+++ src/main.nim
@@ -4,28 +4,28 @@ proc hello*(num: int): int =
   result = num + 4
 
 type
-    Cat = object
+  Cat = object
 
 proc meow(self: Cat) =
-    echo("Meow!")
+  echo("Meow!")
 
 
 let ffxiv_characters = @["Thancred Waters", "Alphinaud Leveilleur", "Hildibrand Manderville"]
 
 proc echoFPSplitList() =
-    # iterate over the list, and echo out the array of split arrays
-    ffxiv_characters.map(
-        (x: string) -> (string, string) => (x.split[0], x.split[1])
-    ).echo
+  # iterate over the list, and echo out the array of split arrays
+  ffxiv_characters.map(
+    (x: string) -> (string, string) => (x.split[0], x.split[1])
+  ).echo
 
 proc echoSplitList() =
-    # iterate over list, and echo each split array on a separate line
-    for name in ffxiv_characters:
-        echo((name.split[0], name.split[1]))
+  # iterate over list, and echo each split array on a separate line
+  for name in ffxiv_characters:
+    echo((name.split[0], name.split[1]))
 
 proc echoJsonSplitList() =
 
-    let list = ffxiv_characters.map(
-        (x: string) -> (string, string) => (x.split[0], x.split[1])
-    )
-    echo ($$list);
+  let list = ffxiv_characters.map(
+    (x: string) -> (string, string) => (x.split[0], x.split[1])
+  )
+  echo ($$list);
--- tests/test1.nim
+++ tests/test1.nim
@@ -2,18 +2,18 @@ include "std/unittest"
 include "../src/main"
 
 suite "first test suite":
-    
-    test "hello fn test":
-        doAssert hello(1) == 5
-        doAssert hello(3) == 7
 
-    test "cat meow test":
-        let cat = Cat()
-        cat.meow()
+  test "hello fn test":
+    doAssert hello(1) == 5
+    doAssert hello(3) == 7
 
-    test "split list":
-        echoSplitList()
-    test "fp split list":
-        echoFPSplitList()
-    test "json split list":
-        echoJsonSplitList()
+  test "cat meow test":
+    let cat = Cat()
+    cat.meow()
+
+  test "split list":
+    echoSplitList()
+  test "fp split list":
+    echoFPSplitList()
+  test "json split list":
+    echoJsonSplitList()
