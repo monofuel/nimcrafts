6b81bec4f1931e45b8fb44ba01dd347e08474be8
monofuel <monofuel@japura.net>
2023-03-18 13:54:00 -0700

    list stuff

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ all: lint test
 
 .PHONY: test
 test:
-	nim c -r tests/*
+	nim c -r tests/* "*"
 
 .PHONY: lint
 lint:
--- src/main.nim
+++ src/main.nim
@@ -1,4 +1,31 @@
-echo "Hello World"
+import sequtils, future, strutils, marshal
 
 proc hello*(num: int): int =
   result = num + 4
+
+type
+    Cat = object
+
+proc meow(self: Cat) =
+    echo("Meow!")
+
+
+let ffxiv_characters = @["Thancred Waters", "Alphinaud Leveilleur", "Hildibrand Manderville"]
+
+proc echoFPSplitList() =
+    # iterate over the list, and echo out the array of split arrays
+    ffxiv_characters.map(
+        (x: string) -> (string, string) => (x.split[0], x.split[1])
+    ).echo
+
+proc echoSplitList() =
+    # iterate over list, and echo each split array on a separate line
+    for name in ffxiv_characters:
+        echo((name.split[0], name.split[1]))
+
+proc echoJsonSplitList() =
+
+    let list = ffxiv_characters.map(
+        (x: string) -> (string, string) => (x.split[0], x.split[1])
+    )
+    echo ($$list);
