7e1ae52d68e0b656283166261740a242c2c4807e
monofuel <monofuel@japura.net>
2023-05-13 14:55:22 -0700

    fix tabs

--- src/screeps/bot/main.nim
+++ src/screeps/bot/main.nim
@@ -6,61 +6,63 @@ var console {.importc, nodecl.}: JsObject
 var module {.importc, nodecl.}: JsObject
 
 proc spawnHarvester1() =
-    let spawnName = "Spawn1".cstring
-    let creepName = "Harvester1".cstring
-    let gameSpawn = game.spawns[spawnName]
+  let
+    spawnName = "Spawn1".cstring
+    creepName = "Harvester1".cstring
+    gameSpawn = game.spawns[spawnName]
 
-    let ret = gameSpawn.spawnCreep([cstring($WORK), cstring($CARRY), cstring(
-            $MOVE)], creepName);
-    console.log("game spawn return code: ".cstring, $ret)
+  let ret = gameSpawn.spawnCreep([cstring($WORK), cstring($CARRY), cstring(
+    $MOVE)], creepName);
+  console.log("game spawn return code: ".cstring, $ret)
 
 
 proc harvesterCreep1() =
-    # module.exports.loop = function () {
-    #     var creep = Game.creeps['Harvester1'];
-    #     var sources = creep.room.find(FIND_SOURCES);
-    #     if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-    #         creep.moveTo(sources[0]);
-    #     }
-    # }
-    let creepName = "Harvester1".cstring
-    let creep: Creep = game.creeps[creepName]
+  # module.exports.loop = function () {
+  #     var creep = Game.creeps['Harvester1'];
+  #     var sources = creep.room.find(FIND_SOURCES);
+  #     if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+  #         creep.moveTo(sources[0]);
+  #     }
+  # }
+  let
+    creepName = "Harvester1".cstring
+    creep: Creep = game.creeps[creepName]
 
-    # TODO making a copy of room breaks things
-    # this compiles to nimCopy() which is not what I want
-    # let room = creep.room
+  # TODO making a copy of room breaks things
+  # this compiles to nimCopy() which is not what I want
+  # let room = creep.room
 
-    let sources: seq[Source] = creep.room.find(FIND_SOURCES)
-    # check length of sources
-    if (len(sources) == 0):
-        console.log("no sources found".cstring)
-        return
-    if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE):
-        let res = creep.moveTo(sources[0].pos)
-        if (res != OK):
-            console.log("moveTo returned: ".cstring, $res,
-                    " for creep: ".cstring, creepName)
+  let sources: seq[Source] = creep.room.find(FIND_SOURCES)
+  # check length of sources
+  if (len(sources) == 0):
+    console.log("no sources found".cstring)
+    return
+  if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE):
+    let res = creep.moveTo(sources[0].pos)
+    if (res != OK):
+      console.log("moveTo returned: ".cstring, $res,
+              " for creep: ".cstring, creepName)
 
-    # TODO next steps
-    #     module.exports.loop = function () {
-    #     var creep = Game.creeps['Harvester1'];
+  # TODO next steps
+  #     module.exports.loop = function () {
+  #     var creep = Game.creeps['Harvester1'];
 
-    #     if(creep.store.getFreeCapacity() > 0) {
-    #         var sources = creep.room.find(FIND_SOURCES);
-    #         if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-    #             creep.moveTo(sources[0]);
-    #         }
-    #     }
-    #     else {
-    #         if( creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE ) {
-    #             creep.moveTo(Game.spawns['Spawn1']);
-    #         }
-    #     }
-    # }
+  #     if(creep.store.getFreeCapacity() > 0) {
+  #         var sources = creep.room.find(FIND_SOURCES);
+  #         if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+  #             creep.moveTo(sources[0]);
+  #         }
+  #     }
+  #     else {
+  #         if( creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE ) {
+  #             creep.moveTo(Game.spawns['Spawn1']);
+  #         }
+  #     }
+  # }
 
 proc mainLoop() =
-    # spawnHarvester1()
-    harvesterCreep1()
+  # spawnHarvester1()
+  harvesterCreep1()
 
 # main()
 module.exports.loop = mainLoop
